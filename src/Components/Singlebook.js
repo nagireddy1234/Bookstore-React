20f275076f9a6c55b9bdafcb31fd4d398b7e4d5a
Yari Andres <yari.andres@gmail.com>
2019-07-31 17:30:09 +0200

    merging

--- src/Components/Singlebook.js
+++ src/Components/Singlebook.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+
 
 class SingleBook extends Component {
-  render() {
-    return (
+  state = {  }
+
+  render() { 
+    return (    
       <div>
+      <Card>
+        <CardImg top width="100%" src="" alt="Card image cap" />
+        <CardBody>
+          <CardTitle></CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
         
-      </div>
-    )
-  } 
+     
+    );
+  }
 
 }
 
--- src/Home.js
+++ src/Home.js
@@ -4,38 +4,37 @@ import Welcome from "./Components/Welcome";
 import FooterPage from "./Components/Footer";
 import Warnignsign from "./Components/Alert";
 import Mybadge from "./Components/Mybadge";
-import Fantasy from './assests/horror.json';
+import Horror from './assests/horror.json';
 import SingleBook from './Components/SingleBook';
 
 export default class Home extends Component {
 
- state = {
-
-   }
+ state = {}
 
   createText = () => {
     return "creating WarningSign which receives as a property a text. This text should be presented inside an Alert of type danger"
   }
 
-  singleBook = () => {
-    Fantasy.slice(0, 1).map((film, index) => {
-      return film;    
-    });    
+  showBook = () => {
+    const details = Horror.slice(0, 1);
+    details.map((film) => {
+      return film.title;
+    }) 
   }
+      
 
-  
+  render() {
 
-  render() { 
     return ( 
       <div >
-      <Menu />
-      <Welcome />
-      <Warnignsign text={this.createText()} />
-      <Mybadge text={this.createText()}/>
-      
-      <SingleBook />
-      <FooterPage />
-    </div>
+        <Menu />
+        <Welcome />
+        <Warnignsign text={this.createText()} />
+        <Mybadge text={this.createText()}/>
+        <SingleBook title={this.showBook()} />
+
+        <FooterPage />
+      </div>
      );
   }
 }
